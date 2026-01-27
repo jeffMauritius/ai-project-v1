d84ccc17c8414a76f40423dba2531f3f39449532
jr <jr@AMW-WKS-JRA-1.myguest.virtualbox.org>
2026-01-27 10:28:21 +0400

    fix devis form

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -40,6 +40,13 @@ const DialogContent = React.forwardRef<
           "fixed left-[50%] top-[50%] z-[9999] grid w-[calc(100%-2rem)] sm:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-gray-200 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg dark:border-gray-800 dark:bg-gray-950 max-h-[90vh] overflow-y-auto",
           className
         )}
+        onInteractOutside={(e) => {
+          // Empêcher la fermeture si on clique sur un Select
+          const target = e.target as HTMLElement;
+          if (target.closest('[role="listbox"]') || target.closest('[data-radix-select-content]')) {
+            e.preventDefault();
+          }
+        }}
         {...props}
       >
         {children}
